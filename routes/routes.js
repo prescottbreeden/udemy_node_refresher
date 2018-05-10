09f68d8b59b7905f6e05f2cae59bbbd004d9e0a2
prescott breeden <pb@prescotts-iMac.local>
2018-05-10 00:07:06 -0700

    udemy notes for angular 6

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,9 @@
 var controller = require('../server/controllers')
-var User = require('../models/user');
 
 module.exports = function(app) {
     
     app.get('/', function(req, res) {
         controller.root(req, res);
-    })
-
-    app.post('/process', function(req, res) {
-        controller.process(req, res);
     })
 }
 
--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __di
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs')
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
